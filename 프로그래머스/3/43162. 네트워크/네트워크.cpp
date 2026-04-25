c452a689962c2a69eca39266c178663a727c2644
eunjung <jjonjunggame@gmail.com>
2026-04-25 10:33:23 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+void dfs(int current, int n, vector<vector<int>>& computers, vector<bool>& visited)
+{
+    visited[current] = true;
+
+    for (int next = 0; next < n; next++)
+    {
+        if (computers[current][next] == 1 && !visited[next])
+        {
+            dfs(next, n, computers, visited);
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers)
+{
+    int answer = 0;
+    vector<bool> visited(n, false);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            answer++;
+            dfs(i, n, computers, visited);
+        }
+    }
+
+    return answer;
+}
